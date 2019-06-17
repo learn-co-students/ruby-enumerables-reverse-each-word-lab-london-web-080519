6dc59fb72d925685df03f86d6ec2aaa868e2ef14
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-17 19:49:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,14 @@
+#USING EACH
+# def reverse_each_word(str)
+#   answer = []
+#   arr = str.split(" ")
+#   arr.each do |word|
+#     answer << word.reverse
+#   end
+#   answer.join(" ")
+# end
+
+#USING COLLECT
 def reverse_each_word(str)
   answer = []
   arr = str.split(" ")
