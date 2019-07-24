1dc53a7678f5bd6d173ffce387e9ee9b35b4f001
Sarah Drake <jussaradrake@outlook.com>
2019-07-24 16:13:20 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,9 +9,9 @@ end
 
 def reverse_each_word(string)
   array = string.split(" ") #turn string into an array
-  test_array = []
+  test_array = [] #empty array
   array.collect do|string| #iterate over the array
-    test_array << string.reverse #reverse each word in the array
+    test_array << string.reverse #push and reverse each word in the array
   end
   test_array.join(" ")
 end
